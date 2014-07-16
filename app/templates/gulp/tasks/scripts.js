ceb3399585109860ed88ec9deba2f45737408015
Joshua Roof <josh.roof@gmail.com>
2014-07-16 00:23:02 -0400

    catch browserify and coffeescript errors

--- app/templates/gulp/tasks/scripts.js
+++ app/templates/gulp/tasks/scripts.js
@@ -10,11 +10,11 @@ var gulp = require('gulp'),
 gulp.task('scripts', function () {
 
   return gulp.src(globals.appPath + '/assets/javascripts/App.coffee', {read: false})
+    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
     .pipe(browserify({
       transform: ['browserify-handlebars', 'coffeeify'],
       extensions: ['.coffee']
     }))
-    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
     .pipe(rename('bundle.js'))
     .pipe(gulp.dest(globals.appPath + '/_tmp/javascripts'))
     .pipe(notify('Gulp successfully compiled your coffeescript files!'));
